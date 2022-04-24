8682852938d145bdd2ec3d8a432044b4ecb6923a
Tifani <tiffany.dermendzhieva@abv.bg>
2022-04-24 13:27:26 +0300

    added render function to app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('hi')
+const rootElement = document.getElementById('root');
+
+function renderGameField(gameField) {
+
+    let gameFieldCopy = gameField.slice();
+
+
+    const tableElement = document.createElement('table');
+    for (let i = 0; i < 3; i++) {
+        const tableRow = document.createElement('tr');
+
+        for (let j = 0; j < 3; j++) {
+
+            const tableData = document.createElement('td');
+
+            let currentField = gameFieldCopy.shift();
+            tableData.textContent = currentField;
+            tableRow.appendChild(tableData);
+        }
+
+        tableElement.appendChild(tableRow)
+
+    }
+
+    rootElement.replaceChildren(tableElement);
+
+}
+
+let input = ["X", "X", "X", "O", "O ", "X", "X", "X", "O"];
+
+renderGameField(input);
+
+
+
+
+
